3b9cd4b7823c20d03c6b9bec7085d35fca9c6f40
seliana <cany96@bk.ru>
2022-03-09 20:19:50 +0400

    chore: config overrided to add aliases

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@style': path.resolve(__dirname, 'src/style'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+};
